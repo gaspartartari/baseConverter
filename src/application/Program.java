20ec424aaf5d9b6dc085d270dc5d585dcbe47630
Gaspar Tartari <gaspartartari@gmail.com>
2023-10-15 20:54:07 -0300

    Bug fixes and logic improvment

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,101 +11,99 @@ public class Program{
     
         Scanner s = new Scanner(System.in);
         
-        int inicio = 0;
+        int init = 0;
 
-             while (inicio != 7){
+             while (init != 7){
                 try{
-                    switch (inicio){
+                    switch (init){
                         case 0:
-                            System.out.println("\nDigite a opção desejada:");
-                            System.out.println("1 | decimal para binário");
+                            System.out.println("\nEnter the desired option:");
+                            System.out.println("1 | decimal to binary");
                             System.out.println("2 | binário para decimal");
-                            System.out.println("3 | hexadecimal para decimal");
-                            System.out.println("4 | decimal para hexadecimal");
-                            System.out.println("5 | hexadecimal para binário");
-                            System.out.println("6 | binario para hexadecimal");
-                            System.out.println("7 | sair");
+                            System.out.println("3 | hexadecimal to decimal");
+                            System.out.println("4 | decimal to hexadecimal");
+                            System.out.println("5 | hexadecimal to binary");
+                            System.out.println("6 | binary para hexadecimal");
+                            System.out.println("7 | END");
                             int x = s.nextInt();
                             s.nextLine();
                             
                             switch (x){
                                 case 1:
-                                    inicio = 1;
+                                    init = 1;
                                     break;
                                 case 2:
-                                    inicio = 2;
+                                    init = 2;
                                     break;
                                 case 3:
-                                    inicio = 3;
+                                    init = 3;
                                     break;
                                 case 4:
-                                    inicio = 4;
+                                    init = 4;
                                     break;
                                 case 5:
-                                    inicio = 5;
+                                    init = 5;
                                     break;
                                 case 6:
-                                    inicio = 6;
+                                    init = 6;
                                     break;
                                 case 7:
-                                    inicio = 7;
+                                    init = 7;
                                     break;
                                 default:
-                                    System.out.println("\nOpção Inválida\n");
-                                    inicio = 0;
+                                    System.out.println("\nInvalid option\n");
+                                    init = 0;
                                     break; 
                             }
                             break;
                         
                         case 1:
-                            System.out.println("\nDigite um numero decimal:");
+                            System.out.println("\nEnter a decimal number:");
                             long decimal = s.nextLong();
                             s.nextLine();
-                            long resposta = Calculator.decimalToBinary(decimal);
-                            System.out.println("\nBinário: " + resposta);
-                            inicio = 0;
+                            System.out.println("\nBinary: " + Calculator.decimalToBinary(decimal));
+                            init = 0;
                             break;
                         
                         case 2:
-                            System.out.println("\nDigite um numero binario:");
-                            String binario = s.nextLine();
-                            String resposta2 = Calculator.binarioParaDecimal(binario);
-                            System.out.println("\nDecimal: " + resposta2);
-                            inicio = 0;
+                            System.out.println("\nEnter a binary number:");
+                            String binary = s.nextLine();
+                            System.out.println("\nDecimal: " + Calculator.binaryToDecimal(binary));
+                            init = 0;
                             break;
     
                         case 3:
-                            System.out.println("\nDigite um numero hexadecimal:");
+                            System.out.println("\nEnter a hexadecimal number:");
                             String hexa = s.nextLine();
-                            System.out.println("\nDecimal: " + Calculator.hexaDecimalParaDecimal(hexa));
-                            inicio = 0;
+                            System.out.println("\nDecimal: " + Calculator.hexadecimalToDecimal(hexa));
+                            init = 0;
                             break;
                             
                         case 4:
-                            System.out.println("\nDigite um numero decimal:");
-                            int d = s.nextInt();
+                            System.out.println("\nEnter a decimal number:");
+                            int decimal2 = s.nextInt();
                             s.nextLine();
-                            System.out.println("\nHexadecimal: " + Calculator.decimalParaHexadecimal(d));
-                            inicio = 0;
+                            System.out.println("\nHexadecimal: " + Calculator.decimalToHexadecimal(decimal2));
+                            init = 0;
                             break;
                         
                         case 5:
-                            System.out.println("\nDigite um numero hexadecimal:");
-                            String h = s.nextLine();
-                            System.out.println("\nBinário: " + Calculator.hexadecimalParaBinario(h));
-                            inicio = 0;
+                            System.out.println("\nEnter a hexadecimal number:");
+                            String hexaDecimal = s.nextLine();
+                            System.out.println("\nBinary: " + Calculator.hexadecimalToBinary(hexaDecimal));
+                            init = 0;
                             break;
                             
                         case 6:
-                            System.out.println("\nDigite um numero binário:");
+                            System.out.println("\nEnter a binary number:");
                             String bin = s.nextLine();
-                            System.out.println("\nHexadecimal: " + Calculator.binarioParaHexadecimal(bin));
-                            inicio = 0;
+                            System.out.println("\nHexadecimal: " + Calculator.binaryToHexadecimal(bin));
+                            init = 0;
                             break;
                             
                         default:
-                            System.out.println("\nOpção inválida\n");
-                            inicio = 6;
+                            System.out.println("\nInvalid Option\n");
+                            init = 6;
                             break;
                     }   
                 }
@@ -113,17 +111,22 @@ public class Program{
                     System.out.println();
                     System.out.println("Invalid input. Please enter a valid option.");
                     s.nextLine();
-                    inicio = 0;
+                    init = 0;
                 }
                 catch (InputMismatchBinary e){
                     System.out.println();
                     System.out.println(e.getMessage());
-                    inicio = 0;
+                    init = 0;
                 }
                 catch (InputMismatchHexadecimal e){
                     System.out.println();
                     System.out.println(e.getMessage());
-                    inicio = 0;
+                    init = 0;
+                }
+                catch (IllegalArgumentException e){
+                    System.out.println();
+                    System.out.println(e.getMessage());
+                    init = 0;
                 }
                 
             }
--- src/model/util/Calculator.java
+++ src/model/util/Calculator.java
@@ -5,132 +5,77 @@ import model.exceptions.InputMismatchHexadecimal;
 
 public class Calculator{
 
-    public static long decimalToBinary(long decimal) {
+    public static String decimalToBinary(long decimal) {
 
-        String ret = "";
+        StringBuilder binary = new StringBuilder();
+
         while(decimal != 0){
-            if(decimal % 2 == 0){
-                decimal = decimal / 2;
-                long longDecimal = (long) decimal;
-                decimal = longDecimal;
-                ret +="0";
-            }else{
-                decimal = decimal / 2;
-                long longDecimal = (long) decimal;
-                decimal = longDecimal;
-                ret +="1";
-            }
-        }
-        
-        char[] caracteres = ret.toCharArray();
-        int inicio = 0;
-        int fim = caracteres.length -1;
-        
-        while (inicio < fim){
-            char temp = caracteres[inicio];
-            caracteres[inicio] = caracteres[fim];
-            caracteres[fim] = temp;
-            inicio++;
-            fim--;
-        }
-        
-        ret = new String (caracteres);
-        long retornoLong = Long.parseLong(ret);
-        return retornoLong;
-        
+            binary.insert(0, decimal % 2);
+            decimal /=2;
+        } 
+        return binary.toString();
     }
     
-    public static String binarioParaDecimal(String binario) throws InputMismatchBinary{
+    public static String binaryToDecimal(String binary) throws InputMismatchBinary{
 
-        char[] verificaBinario = binario.toCharArray();
-        for (int a = 0 ; a < verificaBinario.length; a++)
-            if(verificaBinario[a] != '0' && verificaBinario[a] != '1')
-                throw new InputMismatchBinary("Invalid binary input. Please enter a valid option");
+        char[] verifyBinary = binary.toCharArray();
+        for (int a = 0 ; a < verifyBinary.length; a++)
+            if(verifyBinary[a] != '0' && verifyBinary[a] != '1')
+                throw new InputMismatchBinary("Invalid binary input: " + verifyBinary[a] + ". Please enter a valid option");
             
-            char[] cBinario = binario.toCharArray();
+            char[] arrayBinary = binary.toCharArray();
             int base = 2;
-            int expoente = 0;
-            int somaFinal = 0;
+            int expoent = 0;
+            int finalSum = 0;
             double x = 0;
-            double resultado = 0;
+            double result = 0;
             
-            for(int i = cBinario.length-1; i >= 0 ; i--){
-                x = Math.pow (base, expoente);
-                String cI = String.valueOf(cBinario[i]);
-                double cId = Double.parseDouble(cI);
+            for(int i = arrayBinary.length-1; i >= 0 ; i--){
+                x = Math.pow (base, expoent);
+                String stringValue = String.valueOf(arrayBinary[i]);
+                double doubleValue = Double.parseDouble(stringValue);
                 
-                resultado = cId * x;
-                somaFinal += resultado;
-                expoente +=1;
+                result = doubleValue * x;
+                finalSum += result;
+                expoent +=1;
             }
             
-            String r = String.valueOf(somaFinal);
-            return r;
+            String finalReturn = String.valueOf(finalSum);
+            return finalReturn;
             
     }
     
-    public static int hexaDecimalParaDecimal(String hexaDecimal) throws InputMismatchHexadecimal{
-        char [] cHexaDecimal = hexaDecimal.toCharArray();
-        for(char a : cHexaDecimal){
-            if(a != '1' || a !='2' || a != '3' || a != '4' || a != '5' || a != '6' || a != '7' || a != '8' || a != '9'
-                     || Character.toString(a).equalsIgnoreCase("a") || Character.toString(a).equalsIgnoreCase("b") 
-                           || Character.toString(a).equalsIgnoreCase("c") || Character.toString(a).equalsIgnoreCase("d")
-                               || Character.toString(a).equalsIgnoreCase("e") || Character.toString(a).equalsIgnoreCase("f"))
-                                    {
-                                     throw new InputMismatchHexadecimal("Invalid input. Please enter a valid hexadecimal value.");
-                                    }
-        }
-
+    public static int hexadecimalToDecimal(String hexadecimal) throws InputMismatchHexadecimal {
+        char[] arrayDecimal = hexadecimal.toCharArray();
         int base = 16;
-        int expoente = 0;
-        double mathRound = 0;
-        double resultado = 0;
-        int retorno = 0;
-        for(int i = cHexaDecimal.length-1; i >= 0; i--){
-            if(cHexaDecimal[i] == 'a' || cHexaDecimal[i] == 'A' ){
-                mathRound = Math.pow(base, expoente);
-                resultado = 10 * mathRound;
-                retorno += resultado;
-                expoente +=1;
-            }else if(cHexaDecimal[i] == 'b' || cHexaDecimal[i] == 'B'){
-                mathRound = Math.pow(base, expoente);
-                resultado = 11 * mathRound;
-                retorno += resultado;
-                expoente +=1;
-            }else if(cHexaDecimal[i] == 'c'|| cHexaDecimal[i] == 'C'){
-                mathRound = Math.pow(base, expoente);
-                resultado = 12 * mathRound;
-                retorno += resultado;
-                expoente +=1;
-            }else if(cHexaDecimal[i] == 'd' || cHexaDecimal[i] == 'D'){
-                mathRound = Math.pow(base, expoente);
-                resultado = 13 * mathRound;
-                retorno += resultado;
-                expoente +=1;
-            }else if(cHexaDecimal[i] == 'e' || cHexaDecimal[i] == 'E'){
-                mathRound = Math.pow(base, expoente);
-                resultado = 14 * mathRound;
-                retorno += resultado;
-                expoente +=1;
-            }else if(cHexaDecimal[i] == 'f' || cHexaDecimal[i] == 'F'){
-                mathRound = Math.pow(base, expoente);
-                resultado = 15 * mathRound;
-                retorno += resultado;
-                expoente +=1;
-            }else{
-                mathRound = Math.pow(base, expoente);
-                String charToInt = String.valueOf(cHexaDecimal[i]);
-                double intToDouble = Double.parseDouble(charToInt);
-                resultado = intToDouble * mathRound;
-                retorno += resultado;
-                expoente +=1;
+        int ret = 0;
+        int expoent = 0;
+    
+        for (int i = arrayDecimal.length - 1; i >= 0; i--) {
+            char c = Character.toLowerCase(arrayDecimal[i]); 
+    
+            int value = -1; 
+    
+            if (c >= '0' && c <= '9') {
+                value = c - '0';
+            } 
+            else if (c >= 'a' && c <= 'f') {
+                value = c - 'a' + 10; 
             }
+    
+            if (value == -1) {
+                throw new InputMismatchHexadecimal("Invalid hexadecimal input: " + arrayDecimal[i] + ". Please enter a option.");
+            }
+    
+            ret += value * Math.pow(base, expoent);
+            expoent += 1;
         }
-            
-        return retorno;
+    
+        return ret;
     }
     
-    public static String decimalParaHexadecimal(int decimal){
+    
+    public static String decimalToHexadecimal(int decimal){
         int count = 0;
         int temp = decimal;
         while (temp != 0){
@@ -138,148 +83,59 @@ public class Calculator{
             count++;
         }
         
-        char[] restos = new char[count];
+        char[] remainders = new char[count];
         
         char[] hexas ={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         
         for(int i = 0; i < count; i ++){
-            int resto = decimal % 16;
-            restos[count - 1 - i] = hexas[resto];
+            int remainder = decimal % 16;
+            remainders[count - 1 - i] = hexas[remainder];
             decimal = decimal / 16; 
         }
         
-        return new String (restos);
+        return new String (remainders);
     }
     
-    public static String hexadecimalParaBinario(String hexa) throws InputMismatchHexadecimal{
-        
-        char[] hexaChar = hexa.toCharArray();
-        for(char a : hexaChar){
-            if(a != '1' || a !='2' || a != '3' || a != '4' || a != '5' || a != '6' || a != '7' || a != '8' || a != '9'
-                     || Character.toString(a).equalsIgnoreCase("a") || Character.toString(a).equalsIgnoreCase("b") 
-                           || Character.toString(a).equalsIgnoreCase("c") || Character.toString(a).equalsIgnoreCase("d")
-                               || Character.toString(a).equalsIgnoreCase("e") || Character.toString(a).equalsIgnoreCase("f"))
-                                    {
-                                     throw new InputMismatchHexadecimal("Invalid input. Please enter a valid hexadecimal value.");
-                                    }
+    public static String hexadecimalToBinary(String hex) {
+        hex = hex.toUpperCase();
+    
+        StringBuilder binary = new StringBuilder();
+    
+        for (int i = 0; i < hex.length(); i++) {
+            char c = hex.charAt(i);
+    
+            if (Character.isDigit(c)) {
+                int decimalValue = Character.getNumericValue(c);
+                binary.append(String.format("%4s", Integer.toBinaryString(decimalValue)).replace(' ', '0'));
+            } else if (c >= 'A' && c <= 'F') {
+                int decimalValue = c - 'A' + 10;
+                binary.append(String.format("%4s", Integer.toBinaryString(decimalValue)).replace(' ', '0'));
+            } else {
+                throw new IllegalArgumentException("Invalid hexadecimal input: " + c + ". Please enter a valid option");
+            }
         }
-        String hexaUpperCase = hexa.toUpperCase();
-        String binario = "";
-        for(int i = 0; i < hexaChar.length; i++){
-            char c = Character.toUpperCase(hexaChar[i]);
-            if (Character.isDigit(hexaChar[i])){
-                switch (c){
-                    case '0':
-                        binario +="0000";
-                        break;
-                    case '1':
-                        binario +="0001";
-                        break;
-                    case '2':
-                        binario +="0010";
-                        break;
-                    case '3':
-                        binario +="0011";
-                        break;
-                    case '4':
-                        binario +="0100";
-                        break;
-                    case '5':
-                        binario +="0101";
-                        break;
-                    case '6':
-                        binario +="0110";
-                        break;
-                    case '7':
-                        binario +="0111";
-                        break;
-                    case '8':
-                        binario +="1000";
-                        break;
-                    case '9':
-                        binario +="1001";
-                         break;
-                    default:
-                        throw new IllegalArgumentException("O valor binário " + hexaChar[i] + " é inválido");
-                }
-            }else if (hexaChar[i] >= 'A' || hexaChar[i] <= 'F'){
-                switch (hexaChar[i]){      
-                    case 'A':
-                        binario +="1010";
-                        break;
-                    case 'B':
-                        binario +="1011";
-                        break;
-                    case 'C':
-                        binario +="1100";
-                        break;
-                    case 'D':
-                        binario +="1101";
-                        break;
-                    case 'E':
-                        binario +="1110";
-                        break;
-                    case 'F':
-                        binario +="1111";
-                        break;
-                }    
-            }else
-                throw new IllegalArgumentException("O valor binário " + hexaChar[i] + " é inválido");
-        }
-        
-        String binarioS = String.valueOf(binario);
-        
-        char[] verifica = binarioS.toCharArray();
-        
-        for (int i = 0; i < verifica.length; i++)
-            if(verifica[i] == '0')
-                verifica [i] = '\0';
-            else
-                break;        
-         
-        return String.valueOf(verifica);
+    
+        return binary.toString();
     }
     
-    public static String binarioParaHexadecimal(String binario) throws InputMismatchBinary{
-        char[] verificaBinario = binario.toCharArray();
-        for (int a = 0 ; a < verificaBinario.length; a++)
-            if(verificaBinario[a] != '0' && verificaBinario[a] != '1')
-                throw new InputMismatchBinary("Invalid binary input. Please enter a valid option");
+   public static String binaryToHexadecimal(String binary) throws InputMismatchBinary {
+    char[] isValidBinary = binary.toCharArray();
+    for (int a = 0; a < isValidBinary.length; a++) {
+        if (isValidBinary[a] != '0' && isValidBinary[a] != '1') {
+            throw new InputMismatchBinary("Invalid binary input: " + isValidBinary[a] + ". Please enter a valid option");
+        }
+    }
 
-        char[] cBinario = binario.toCharArray();
-        int base = 2;
-        int expoente = 0;
-        double resultado = 0;
-        int soma = 0;
-        double mP = 0;
-        
-        for(int i = cBinario.length-1; i >= 0; i--){
-            mP = Math.pow(base, expoente);
-            String rS = String.valueOf(cBinario[i]);
-            double rD = Double.parseDouble(rS);
-            resultado = rD * mP;
-            soma += resultado;
-            expoente++;
-        }
-        
-        int count = 0;
-        int temp = soma;
-        
-        while(temp != 0){
-            temp = temp / 16;
-            count++;
-        }
-        
-        char[] restos = new char[count];
-        char[] hexas ={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-        
-        for(int i = 0; i < count; i ++){
-            int resto = soma % 16;
-            restos[count - 1 - i] = hexas[resto];
-            soma = soma / 16; 
-        }
-        
-        return new String (restos);
+    int decimalValue = 0;
+
+    for (int i = 0; i < binary.length(); i++) {
+        char bit = binary.charAt(i);
+        decimalValue = (decimalValue << 1) + (bit - '0');
+    }
+
+    String hexadecimal = Integer.toHexString(decimalValue).toUpperCase();
+    return hexadecimal;
     }
 }
+
 
